86405e3b45ba00c3f353be49e371f1702718409c
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2024-02-11 19:58:45 +0100

    Fix header size for mobile

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,7 @@
 import { FileJson2 } from "lucide-react";
 
 const Header = () => (
-  <h1 className="text-2xl font-bold flex gap-3">
+  <h1 className="text-2xl font-bold flex gap-3 scale-75 sm:scale-100">
     <span className="px-2 pt-1 border-t-4 dark:pt-0 dark:border-t-0 dark:border-b-4 transition-all">
       JSON
     </span>
